06a7cca96842e0b2108650cca4567db12cedc9f0
unknown <hassanahmat41gmail.com>
2024-05-28 21:23:07 +0100

    Changement du fichier de la base de donnée

--- database/cee_db.sql
+++ database/cee_db.sql
@@ -3,8 +3,8 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : jeu. 28 déc. 2023 à 11:23
--- Version du serveur : 10.4.21-MariaDB
+-- Généré le : mar. 28 mai 2024 à 22:20
+-- Version du serveur : 10.6.17-MariaDB
 -- Version de PHP : 8.0.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -29,9 +29,9 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `admin_acc` (
   `admin_id` int(11) NOT NULL,
-  `admin_user` varchar(1000) NOT NULL,
-  `admin_pass` varchar(1000) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `admin_user` varchar(1000) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `admin_pass` varchar(1000) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Déchargement des données de la table `admin_acc`
@@ -48,17 +48,18 @@ INSERT INTO `admin_acc` (`admin_id`, `admin_user`, `admin_pass`) VALUES
 
 CREATE TABLE `course_tbl` (
   `cou_id` int(11) NOT NULL,
-  `cou_name` varchar(1000) NOT NULL,
+  `cou_name` varchar(1000) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   `cou_created` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Déchargement des données de la table `course_tbl`
 --
 
 INSERT INTO `course_tbl` (`cou_id`, `cou_name`, `cou_created`) VALUES
-(76, 'WEB CLIENT RICHE', '2023-12-26 10:24:31'),
-(78, 'PHYSIQUE ', '2023-12-26 11:27:32');
+(89, 'GESTION DES ENTREPRISES', '2024-05-01 15:45:27'),
+(90, 'POO', '2024-05-01 15:52:20'),
+(92, 'MANAGEMENT COMPANY', '2024-05-01 22:06:47');
 
 -- --------------------------------------------------------
 
@@ -68,24 +69,25 @@ INSERT INTO `course_tbl` (`cou_id`, `cou_name`, `cou_created`) VALUES
 
 CREATE TABLE `examinee_tbl` (
   `exmne_id` int(11) NOT NULL,
-  `exmne_fullname` varchar(1000) NOT NULL,
-  `exmne_course` varchar(1000) NOT NULL,
-  `exmne_gender` varchar(1000) NOT NULL,
-  `exmne_birthdate` varchar(1000) NOT NULL,
-  `exmne_year_level` varchar(1000) NOT NULL,
-  `exmne_email` varchar(1000) NOT NULL,
-  `exmne_password` varchar(1000) NOT NULL,
-  `exmne_status` varchar(1000) NOT NULL DEFAULT 'active'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `exmne_fullname` varchar(1000) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `exmne_course` varchar(1000) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `exmne_gender` varchar(1000) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `exmne_birthdate` varchar(1000) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `exmne_year_level` varchar(1000) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `exmne_email` varchar(1000) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `exmne_password` varchar(1000) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `exmne_status` varchar(1000) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT 'active'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Déchargement des données de la table `examinee_tbl`
 --
 
 INSERT INTO `examinee_tbl` (`exmne_id`, `exmne_fullname`, `exmne_course`, `exmne_gender`, `exmne_birthdate`, `exmne_year_level`, `exmne_email`, `exmne_password`, `exmne_status`) VALUES
-(28, 'Hassan Ahmat', '76', 'male', '2000-01-04', 'second year', 'hassan@gmail.com', '12345678', 'active'),
-(29, 'Kore', '78', 'male', '2023-11-29', 'third year', 'kore@gmail.com', '1234', 'active'),
-(30, 'Idriss', '76', 'male', '2023-11-03', 'second year', 'idriss@gmail.com', '1234', 'active');
+(50, 'Hassan Ahmat Fadoul', '89', 'male', '1998-12-29', 'third year', 'hassan@gmail.com', '1234', 'active'),
+(51, 'Idriss Mht Ali', '90', 'male', '1999-05-05', 'third year', 'idriss@gmail.com', '12345', 'active'),
+(52, 'Ibrahim Idriss', '90', 'male', '2024-05-03', 'third year', 'ibrahim@gmail.com', '12345', 'active'),
+(53, 'Mht Ibrahim', '92', 'male', '2024-05-09', 'first year', 'mht@gmail.com', '1234', 'active');
 
 -- --------------------------------------------------------
 
@@ -97,8 +99,8 @@ CREATE TABLE `exam_attempt` (
   `examat_id` int(11) NOT NULL,
   `exmne_id` int(11) NOT NULL,
   `exam_id` int(11) NOT NULL,
-  `examat_status` varchar(1000) NOT NULL DEFAULT 'used'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `examat_status` varchar(1000) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT 'used'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Déchargement des données de la table `exam_attempt`
@@ -119,20 +121,21 @@ CREATE TABLE `exam_tbl` (
   `titre` varchar(1000) NOT NULL,
   `ex_time_limit` varchar(1000) NOT NULL,
   `contenu` varchar(1000) NOT NULL,
-  `reponse_attendue` text NOT NULL,
-  `reponse_candidat` text NOT NULL,
+  `reponse_attendue` mediumtext NOT NULL,
+  `reponse_candidat` mediumtext NOT NULL,
   `similarite` decimal(5,2) DEFAULT NULL,
-  `submitted` tinyint(4) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `submitted` tinyint(4) NOT NULL,
+  `evaluated` tinyint(4) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Déchargement des données de la table `exam_tbl`
 --
 
-INSERT INTO `exam_tbl` (`ex_id`, `cou_id`, `titre`, `ex_time_limit`, `contenu`, `reponse_attendue`, `reponse_candidat`, `similarite`, `submitted`) VALUES
-(133, 76, 'Examen Web Client Riche', '30', 'Cest quoi le web?', 'Ce sont des pages web', 'ce sont des pages web', NULL, 1),
-(136, 78, 'Examen Physique', '20', 'cest quoi la physique?', 'cest une science', '', '21.75', 1),
-(137, 76, 'Sujet de Web', '10', 'C\'est quoi le web selon un développeur Web ?', 'c\'est l\'ensembles des pages web qui permettent d\'interagir sur le net', 'c\'est l\'ensemble des pages web qui interagissent sur le net.', '81.73', 1);
+INSERT INTO `exam_tbl` (`ex_id`, `cou_id`, `titre`, `ex_time_limit`, `contenu`, `reponse_attendue`, `reponse_candidat`, `similarite`, `submitted`, `evaluated`) VALUES
+(162, 90, 'Examen POO', '20', 'C\'est quoi la POO?', 'C\'est la programmation orienté objet', 'c\'est la programmation orienté objet qui nous facilite lavie nous les devéloppeurs', NULL, 1, 0),
+(163, 92, 'Business Management Exam', '10', 'Describe the importance of a company somewhat', 'The purpose of a company is to create profit for its shareholders. From the point of view of trying to discover the most important function of a company, it\'s the one that\'s creating stakeholder value. Let\'s again take a shortcut and just say that the way to do that is by growing the compan', '', NULL, 0, 0),
+(164, 92, 'Business Management Exam', '10', 'Describe the importance of a company somewhat', 'The purpose of a company is to create profit for its shareholders. From the point of view of trying to discover the most important function of a company, it\'s the one that\'s creating stakeholder value. Let\'s again take a shortcut and just say that the way to do that is by growing the compan', 'The purpose of a company is to create profit for its shareholders. From the perspective of finding the most important function of a business, it is the one that creates value for stakeholders', NULL, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -143,10 +146,10 @@ INSERT INTO `exam_tbl` (`ex_id`, `cou_id`, `titre`, `ex_time_limit`, `contenu`, 
 CREATE TABLE `feedbacks_tbl` (
   `fb_id` int(11) NOT NULL,
   `exmne_id` int(11) NOT NULL,
-  `fb_exmne_as` varchar(1000) NOT NULL,
-  `fb_feedbacks` varchar(1000) NOT NULL,
-  `fb_date` varchar(1000) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `fb_exmne_as` varchar(1000) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `fb_feedbacks` varchar(1000) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `fb_date` varchar(1000) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Index pour les tables déchargées
@@ -202,13 +205,13 @@ ALTER TABLE `admin_acc`
 -- AUTO_INCREMENT pour la table `course_tbl`
 --
 ALTER TABLE `course_tbl`
-  MODIFY `cou_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=79;
+  MODIFY `cou_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=93;
 
 --
 -- AUTO_INCREMENT pour la table `examinee_tbl`
 --
 ALTER TABLE `examinee_tbl`
-  MODIFY `exmne_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+  MODIFY `exmne_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
 
 --
 -- AUTO_INCREMENT pour la table `exam_attempt`
@@ -220,7 +223,7 @@ ALTER TABLE `exam_attempt`
 -- AUTO_INCREMENT pour la table `exam_tbl`
 --
 ALTER TABLE `exam_tbl`
-  MODIFY `ex_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=138;
+  MODIFY `ex_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=165;
 
 --
 -- AUTO_INCREMENT pour la table `feedbacks_tbl`
